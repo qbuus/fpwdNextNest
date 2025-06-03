ca6840a490bfef47b8090ba9951b1aac178152e7
qbuus <jkocz867@gmail.com>
2025-06-03 23:29:04 +0200

    add connection to mongoDb

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGODB_URI');
+        if (!uri) {
+          console.error('Missing MONGODB_URI environment variable');
+          return {
+            uri: 'mongodb://localhost/invalid',
+            connectionFactory: () => null,
+          };
+        }
+
+        console.log('You are now connected to MongoDB');
+        return {
+          uri,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
